3b5cd8059c752cef46028c1cf9e7bcfcbf1aaaeb
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-07-29 11:49:23 -0400

    Summary : AWS DeepRacer init

---
+++ Deep Racer/deep_racer.py
@@ -0,0 +1,26 @@
+'''
+
+chrome > moodys.okta.com > aws
+	   > deepracerrole
+	   
+	   ... deepracer (search)
+
+august 9 / august 18 : regular / playoffs
+
+reinforcement learning
+	stereo image  + LIDAR data
+
+do not click on robomaker access
+
+	feedback - reward
+	state from track
+
+	reward function
+	policy network 
+	
+	15 pics / second = state
+
+	sagemaker to roboracer
+	
+
+'''
