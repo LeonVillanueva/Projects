1d13d936d983d9b3489a82af31b07fd5504e5188
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-08-16 15:10:50 -0400

    Summary : added centering, fixed speed tweak

--- Deep Racer/vgl-reward-final-speed.py
+++ Deep Racer/vgl-reward-final-speed.py
@@ -16,13 +16,17 @@ def reward_function(params):
     heading = params['heading'] # float,  [(0:Max-1),(1:Max-1)]
     is_left = params['is_left_of_center'] # boolean
 
-    SHARP = 15
+    SAFE_TURN = 5
+    MAX_TURN = 30
 
     MIN_SPEED = 1.0
     MID_SPEED = 2.0
     MAX_SPEED = 3.0
 
-    reward = (progress + 1e2)/10
+    reward = (progress + 1e-3) / 10
+
+    if (from_center < 0.2*width) and (progress > 0):
+        reward += progress / 50
 
     waypoint1 = waypoints[closest_waypoints[1]]
     waypoint0 = waypoints[closest_waypoints[0]]
@@ -30,18 +34,23 @@ def reward_function(params):
     way_degree = math.degrees (math.atan2(waypoint1[1] - waypoint0[1], waypoint1[0] - waypoint0[0]))
     diff_degree = abs (way_degree - heading)
 
-    if diff_degree > 30:
-        reward *= 0.5
-    if diff_degree < 10:
-        reward *= 1.5
-
     if speed < MAX_SPEED:
-        reward = 0.5
+        reward *= 0.5
 
     if progress == 100:
-        reward += 1000
+        reward += 100
 
+    w = 1
+    
+    if (diff_degree > SAFE_TURN):
+        if 0 < (1-(diff_degree/MAX_TURN)) < 1:
+            w = (1-diff_degree/MAX_TURN) + 1e-2
+        else:
+            w = 1e-3
+    
+    reward *= w
+    
     if (not all_wheels): # high penalty for being off track
-        reward = 1e4
-
+        reward = 1e-4    
+        
     return float (reward)
