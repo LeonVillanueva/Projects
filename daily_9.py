cc482c8f920e271b13a1339edacaff26656bf6d5
Leon V <48109980+LeonVillanueva@users.noreply.github.com>
2020-10-26 14:57:15 -0400

    Summary : clean-up, file delete, rename

--- daily_9.py
+++
@@ -1,7 +0,0 @@
-'''
-Given an array and a number k that's smaller than the length of the array, rotate the array to the right k elements in-place.
-'''
-
-def rotate(array, k):
-    n = len(array) - k
-    return array[n:] + array[:n]
